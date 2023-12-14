b706aa02b8efd7c6f21899b01b4bc270a65afec5
Vladimir Golubev <vladimirg2107@gmail.com>
2023-12-14 20:29:17 +0300

    Add 2 test commands

--- bot.py
+++ bot.py
@@ -19,8 +19,18 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    dp.message.register(cmd_test2, Command("test2"))
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
